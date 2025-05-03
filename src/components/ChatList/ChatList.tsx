c213210b98cddca9e2a6ff02d9dc7a94133bad7b
Yukiiro-Nite <e.kazuma@gmail.com>
2025-05-03 19:20:40 -0400

    Added style for messages that are no longer in the history window

--- src/components/ChatList/ChatList.tsx
+++ src/components/ChatList/ChatList.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from "react"
 import { Chat } from "../../stores/ChatStore"
 import { compiler } from "markdown-to-jsx"
 import { ChatRoleType } from "../../api/llama.types"
+import classNames from "classnames"
+import './ChatList.css'
 
 export interface ChatListProps {
   chat: Chat
@@ -23,14 +25,28 @@ export const ChatList = ({ chat }: ChatListProps) => {
   }, [chat])
 
   return (
-    <ul key={chat.id}>
+    <ul
+      key={chat.id}
+      className="ChatList"
+    >
       {chat.chatHistory.length === 0 && <li><h2>No chat messages</h2></li>}
-      {chat.chatHistory.map((msg) => {
+      {chat.chatHistory.map((msg, index) => {
         const title = roleTitles[msg.role](chat)
         const createdAt = new Date(msg.createdAt).toLocaleString()
         const contentMd = compiler(msg.content, { wrapper: null })
+        const outsideOfHistoryWindow = (chat.chatHistory.length - index) > chat.chatSettings.historyLength
+        const chatMessageClasses = classNames("ChatMessage", {
+          "excludeHistory": outsideOfHistoryWindow
+        })
+        const messageTitle = outsideOfHistoryWindow
+          ? `This message is out of the history window. Try changing the history length if you want to include this message.`
+          : ""
         return (
-          <li key={`${chat.id}-${msg.createdAt}`}>
+          <li
+            key={`${chat.id}-${msg.createdAt}`}
+            className={chatMessageClasses}
+            title={messageTitle}
+          >
             <h2>{title}</h2>
             <time>{createdAt}</time>
             <div className="ChatMsg">{contentMd}</div>
